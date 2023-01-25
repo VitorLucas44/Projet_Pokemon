4b60c33c6c2f38c0200ea67d3d5679897ba5b857
VitorLucas44 <vitoriosolucas@hotmail.com>
2023-01-25 10:50:41 +0100

    css part1

--- public/js/main.js
+++ public/js/main.js
@@ -9,6 +9,7 @@ class Pokemon {
     }
 }
 
+
 //liste pokemon
 let pkmList = [
 ['Dracaufeu', 'https://www.pokepedia.fr/images/7/78/Sprite_006_XY.png', 360, [
@@ -31,6 +32,7 @@ let pkmList = [
 ]]
 ];
 
+
 //type de pokemon
 let typeMatch = {
     'Dracaufeu': [
@@ -49,3 +51,32 @@ let typeMatch = {
         ['plante', 'water']
     ],
 }
+
+
+//Les faires spawns
+function spawn(bool) {
+    let p = pkmList[Math.floor(Math.random() * pkmList.length)];
+    let pkm = new Pokemon(p[0], p[1], p[2], p[3]);
+    
+    if (bool) {
+        for (i = 0; i < 4; i++) {
+            document.getElementById('m' + i).value = pkm.moves[i][0];
+        }
+    }
+    return pkm;
+}
+
+//pokemon1  
+let pk1 = spawn(true);
+s1 = document.createElement('img');
+s1.src = pk1.sprite;
+document.getElementById('pk1').appendChild(s1);
+document.getElementById('hp1').innerHTML = '<p>HP: ' + pk1.hp + '/' + pk1.fullhp + '</p>';
+
+//pokemon2
+let pk2 = spawn(false);
+s2 = document.createElement('img');
+s2.src = pk2.sprite;
+document.getElementById('pk2').appendChild(s2);
+document.getElementById('hp2').innerHTML = '<p>HP: ' + pk2.hp + '/' + pk2.fullhp + '</p>';
+
